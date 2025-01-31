27d89828d8494a5380bd2739289b695870c1b8fc
Jhaap-Kaur <jhaapjeet@gmail.com>
2025-01-31 22:54:15 +0000

    Add files via upload
    updated with pin attempts message and no of attempts

--- pin_programme_hwk.py
+++ pin_programme_hwk.py
@@ -5,13 +5,18 @@ correct_pin = "1234"
 # no. of attempts allowed
 attempts = 3
 
+# for loop with no of attempts included so programme knows when to stop
 for attempt in range (attempts):
 
     supplied_pin = input("Enter your PIN: ")
 
     if supplied_pin == correct_pin:
-        print ("access granted", "number of attempts=",{attempt +1})
+        print (f"access granted. You suceeded in {attempt +1} attempt")
+        # to format values in an f string, add placeholders {}, a placeholder can contain variables and operations
+        # functions and modifiers to format the value
+        # break statement used here to stop the loop if condition is true
         break
-
-    else:print ("access denied", "number of attempts remaining=",{attempts - attempt - 1})
+    # else statement if pin is incorrect print access denied and include no of attempts remaining
+    # programme ends
+    else:print (f"access granted. You failed access denied:{attempts -attempt -1} attempts left")
 
